4edc8bede7544f071615619cd715d430fbaedf68
Jose Montoya <montoyaguzman7@gmail.com>
2021-03-24 22:57:36 -0600

    FEAT : Algunos ejemplos de test

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -1,5 +1,29 @@
 const text = 'Hola Mundo'
+const fruits = ['manzana', 'banana', 'mango']
+const reverseStr = (str, callback) => {
+    callback(str.split('').reverse().join(''))
+}
 
 test('Contiene "Mundo" ', () => {
     expect(text).toMatch(/Mundo/)
 })
+
+test('exist banana?', () => {
+    expect(fruits).toContain('banana')
+})
+
+test('Mayor que', () => {
+    expect(10).toBeGreaterThan(9)
+})
+
+test('istrue', () => {
+    expect(true).toBeTruthy()
+})
+
+test('Probar un callback', () => {
+    reverseStr('hola', (str) => {
+        expect(str).toBe('aloh')
+    })
+})
+
+
