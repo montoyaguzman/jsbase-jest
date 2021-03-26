6175c159fc27d1085cdb5c1acf215ed09ffcbf05
Jose Montoya <montoyaguzman7@gmail.com>
2021-03-26 00:00:10 -0600

    FEAT : Ejemplo de promesas

--- src/__test__/global.test.js
+++ src/__test__/global.test.js
@@ -3,6 +3,14 @@ const fruits = ['manzana', 'banana', 'mango']
 const reverseStr = (str, callback) => {
     callback(str.split('').reverse().join(''))
 }
+const reverseStr2 = str => {
+    return new Promise((resolve, reject) => {
+        if(!str) {
+            reject(Error('error'))
+        }
+        resolve(str.split('').reverse().join(''))
+    })
+}
 
 test('Contiene "Mundo" ', () => {
     expect(text).toMatch(/Mundo/)
@@ -26,4 +34,21 @@ test('Probar un callback', () => {
     })
 })
 
+test('Proar un promise', () => {
+    return reverseStr2('hola')
+        .then(response => {
+            expect(response).toBe('aloh')
+        })
+})
 
+test('Probar con async', async () => {
+    const string = await reverseStr2('hola')
+    expect(string).toBe('aloh')
+})
+
+beforeAll(() => console.log(' === iniciando suite ==='))
+
+beforeEach(() => console.log(' === antes de cada prueba ==='))
+afterEach(() => console.log(' === despues de cada prueba ==='))
+
+afterAll(() => console.log(' === suite terminada ==='))
